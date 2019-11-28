0cb60d6ae86fbedecee39c18c5f9882e59f82fe3
aianman4823 <akitoaianman1@gmail.com>
2019-11-28 21:50:26 +0900

    説明追加

--- react-ts-app/src/pages/App.tsx
+++ react-ts-app/src/pages/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +9,10 @@ const App: React.FC = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           初めの部分は多くがいらないと思うからcomponentsとかpagesとかのディレクトリを作って始めてください！
+          <br />
+          <br />
+          cssファイルを別で作るのではなく、style
+          jsxやStyleSheetを利用してcssを記述するように
         </p>
       </header>
     </div>
